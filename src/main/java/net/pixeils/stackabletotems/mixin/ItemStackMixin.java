1671283c01c6ebc257b823cc00bc82939c550762
pixeils <79124407+pixeils@users.noreply.github.com>
2021-08-08 12:11:51 +1000

    Fix Issue
    I forgot to select only the totem item so every item was a 64 stackable

--- src/main/java/net/pixeils/stackabletotems/mixin/ItemStackMixin.java
+++ src/main/java/net/pixeils/stackabletotems/mixin/ItemStackMixin.java
@@ -1,15 +1,23 @@
 package net.pixeils.stackabletotems.mixin;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
+
+    @Shadow public abstract boolean isOf(Item item);
+
     @Inject(method = "getMaxCount", at = @At("HEAD"), cancellable = true)
     private void stackableTotems(CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(64);
+        if (this.isOf(Items.TOTEM_OF_UNDYING)){
+            cir.setReturnValue(64);
+        }
     }
 }
